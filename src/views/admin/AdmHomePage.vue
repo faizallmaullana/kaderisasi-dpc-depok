f7ec2ac730a5d322a28a19020705f82c65903c6e
faizallmaullana <faizalmaulanasekias@gmail.com>
2024-04-12 15:01:47 +0700

    add margin

--- src/views/admin/AdmHomePage.vue
+++ src/views/admin/AdmHomePage.vue
@@ -14,7 +14,7 @@
           Peserta {{ selectedFilter }}</a>
       </span>
 
-      <span v-else style="display: flex; flex-direction: column;">
+      <span v-else style="display: flex; flex-direction: column; margin-bottom: 1em;">
         <h2 style="margin-block-end:0.5em">{{ selectedFilter }} Peserta ({{ filteredPeserta.length }})</h2>
         <a @click="convertJSONtoCSV" style="margin-block-end: 1em">Download Daftar {{ selectedFilter }} Peserta</a>
         <input type="text" v-model="searchByPhone" @input="filterPesertaByPhone" placeholder="Cari Nomor WhatsApp">
