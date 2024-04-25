51ed9c4a5cb4077ffd8629f1c88880e1f78d8d34
faizallmaullana <faizalmaulanasekias@gmail.com>
2024-04-25 13:54:44 +0700

    Maintenance

--- src/router/index.js
+++ src/router/index.js
@@ -2,75 +2,80 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
-    name: 'HomePage',
-    component: () => import('../views/HomePage.vue')
+    path: '/:catchAll(.*)',
+    name: 'NotFoundPage',
+    component: () => import('../views/NotFoundPage.vue')
   },
-  {
-    path: '/portal',
-    name: 'PortalPage',
-    component: () => import('../views/PortalPage.vue')
-  },
-  {
-    path: '/pendaftaran',
-    name: 'PendaftaranPage',
-    component: () => import('../views/PendaftaranPageDitutup.vue')
-  },
-  {
-    path: '/pendaftaran/tester',
-    name: 'TesterPendaftaranPage',
-    component: () => import('../views/TesterPendaftaranPage.vue')
-  },
-  {
-    path: '/status/:phone',
-    name: 'StatusPendaftaranPage',
-    component: () => import('../views/StatusPendaftaranPage.vue')
-  },
-  {
-    path: '/pengumpulan/tugas',
-    name: 'PengumpulanTugasPage',
-    component: () => import('../views/TugasPage.vue')
-  },
+  // {
+  //   path: '/',
+  //   name: 'HomePage',
+  //   component: () => import('../views/HomePage.vue')
+  // },
+  // {
+  //   path: '/portal',
+  //   name: 'PortalPage',
+  //   component: () => import('../views/PortalPage.vue')
+  // },
+  // {
+  //   path: '/pendaftaran',
+  //   name: 'PendaftaranPage',
+  //   component: () => import('../views/PendaftaranPageDitutup.vue')
+  // },
+  // {
+  //   path: '/pendaftaran/tester',
+  //   name: 'TesterPendaftaranPage',
+  //   component: () => import('../views/TesterPendaftaranPage.vue')
+  // },
+  // {
+  //   path: '/status/:phone',
+  //   name: 'StatusPendaftaranPage',
+  //   component: () => import('../views/StatusPendaftaranPage.vue')
+  // },
+  // {
+  //   path: '/pengumpulan/tugas',
+  //   name: 'PengumpulanTugasPage',
+  //   component: () => import('../views/TugasPage.vue')
+  // },
 
-  // auth
+  // // auth
 
-  {
-    path: '/login',
-    name: 'LoginPage',
-    component: () => import('../views/auth/LoginPage.vue')
-  },
-  {
-    path: '/registration',
-    name: 'RegistrationPage',
-    component: () => import('../views/auth/RegistrationPage.vue')
-  },
+  // {
+  //   path: '/login',
+  //   name: 'LoginPage',
+  //   component: () => import('../views/auth/LoginPage.vue')
+  // },
+  // {
+  //   path: '/registration',
+  //   name: 'RegistrationPage',
+  //   component: () => import('../views/auth/RegistrationPage.vue')
+  // },
 
-  // admin
-  {
-    path: '/adm',
-    name: 'AdmHomePage',
-    component: () => import('../views/admin/AdmHomePage.vue')
-  },
-  {
-    path: '/peserta/:phone',
-    name: 'DataPesertaPage',
-    component: () => import('../views/admin/DataPesertaPage.vue')
-  },
-  {
-    path: '/tugas/deskripsi',
-    name: 'DeskripsiTugasPage',
-    component: () => import('../views/admin/DeskripsiTugasPage.vue')
-  },
-  {
-    path: '/database',
-    name: 'DatabaseManagement',
-    component: () => import('../views/admin/DatabaseManagement.vue')
-  },
-  {
-    path: '/passing/page/090us',
-    name: 'PassingPage',
-    component: () => import('../views/auth/PassingPage.vue')
-  }
+  // // admin
+  // {
+  //   path: '/adm',
+  //   name: 'AdmHomePage',
+  //   component: () => import('../views/admin/AdmHomePage.vue')
+  // },
+  // {
+  //   path: '/peserta/:phone',
+  //   name: 'DataPesertaPage',
+  //   component: () => import('../views/admin/DataPesertaPage.vue')
+  // },
+  // {
+  //   path: '/tugas/deskripsi',
+  //   name: 'DeskripsiTugasPage',
+  //   component: () => import('../views/admin/DeskripsiTugasPage.vue')
+  // },
+  // {
+  //   path: '/database',
+  //   name: 'DatabaseManagement',
+  //   component: () => import('../views/admin/DatabaseManagement.vue')
+  // },
+  // {
+  //   path: '/passing/page/090us',
+  //   name: 'PassingPage',
+  //   component: () => import('../views/auth/PassingPage.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="LandingPage">
+    <div class="heroImage">
+      <div class="temaKegiatan">
+        <h2>Memaknai Marhaenisme guna Menghadapi Bonus Demografi</h2>
+      </div>
+      <img src="@/assets/hero.jpg" alt="">
+    </div>
+
+    <article>
+      <h1>Maintenance</h1>
+      <h4>website dapat diakses lagi pada 15.00</h4>
+    </article>
+  </div>
+</template>
+
+<style scoped>
+#LandingPage article h1 {
+  color: var(--red);
+} 
+
+#LandingPage .header h2 {
+  font-size: 25px;
+}
+
+#LandingPage form p {
+  margin-block: none;
+  text-align: end;
+}
+
+#LandingPage .cekPeserta {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+}
+
+.inputBiasa {
+  margin: 0;
+}
+
+.option {
+  display: flex;
+  gap: 20px
+}
+
+form input.pendaftaran {
+  padding: 15px 20px;
+}
+
+form input.cekStatus {
+  background-color: transparent;
+  border-width: 3px;
+  color: var(--red);
+  border-style: solid;
+  border-color: var(--red)
+}
+
+form input.cekStatus:hover {
+  background-color: var(--red);
+  color: var(--white);
+}
+</style>
